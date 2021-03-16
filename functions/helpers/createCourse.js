21b29b349e7a1a6c61330320bd5ab6e8edd6fa3d
Freanu Peria <magnussonfrancis@gmail.com>
2021-03-16 16:32:41 +0800

    initial commit.

---
+++ functions/helpers/createCourse.js
@@ -0,0 +1,9 @@
+const { table } = require('./airtable');
+const formattedReturn = require('./formattedReturn');
+
+module.exports = async (event) => {
+    // TODO: create course
+    const fields = JSON.parse(event.body)
+    const createdCourse = await table.create([{ fields }])
+    return formattedReturn(201, createdCourse)
+};
---
+++ functions/helpers/deleteCourse.js
@@ -0,0 +1,10 @@
+const { table } = require('./airtable');
+const formattedReturn = require('./formattedReturn');
+module.exports = async (event) => {
+    // TODO: delete course
+
+    const {id} = JSON.parse(event.body)
+    const deletedRecord = await table.destroy(id)
+
+    return formattedReturn(200, deletedRecord)
+};
